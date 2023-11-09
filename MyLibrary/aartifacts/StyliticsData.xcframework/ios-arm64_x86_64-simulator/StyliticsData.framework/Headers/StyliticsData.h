f0e5013aac876555c60ad835ed5f349dc12015e8
Parimal Modi <parimalmo@cybage.com>
2023-11-09 16:59:26 +0530

    Initial commit

---
+++ MyLibrary/aartifacts/StyliticsData.xcframework/ios-arm64_x86_64-simulator/StyliticsData.framework/Headers/StyliticsData.h
@@ -0,0 +1,18 @@
+//
+//  StyliticsData.h
+//  StyliticsData
+//
+//  Created by Hrithik Kesharwani on 13/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StyliticsData.
+FOUNDATION_EXPORT double StyliticsDataVersionNumber;
+
+//! Project version string for StyliticsData.
+FOUNDATION_EXPORT const unsigned char StyliticsDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyliticsData/PublicHeader.h>
+
+
---
+++ MyLibrary/aartifacts/StyliticsUI.xcframework/ios-arm64_x86_64-simulator/StyliticsUI.framework/Headers/StyliticsUI.h
@@ -0,0 +1,16 @@
+//
+//  StyliticsUI.h
+//  StyliticsUI
+//
+//  Created by Hrithik Kesharwani on 27/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StyliticsUI.
+FOUNDATION_EXPORT double StyliticsUIVersionNumber;
+
+//! Project version string for StyliticsUI.
+FOUNDATION_EXPORT const unsigned char StyliticsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyliticsUI/PublicHeader.h>
